9efa72b558f59667d4ae1f34ef86aaa01c3b3dc2
Virtical <138823554+Virtical@users.noreply.github.com>
2025-04-13 11:50:38 +0500

    Add some Tests for Heapify and Swap method

--- HeapSort.Tests/HeapifyTests.cs
+++ HeapSort.Tests/HeapifyTests.cs
@@ -5,16 +5,67 @@ namespace HeapSort.Tests;
 public class HeapifyTests
 {
     [Test]
-    public void WhenHeapifyCalledOnSimpleHeap_ShouldMaintainHeapProperty()
+    public void WhenHeapifyCalledOnAlreadyValidHeap_ShouldNotSwap()
     {
         int ifCount = 0, swapCount = 0;
-        int[] array = [10, 15, 5];
+        int[] array = [20, 10, 15];
 
         var result = HeapSortAlgorithm.Heapify(array, 3, 0, ref ifCount, ref swapCount);
 
+        Assert.That(result, Is.EqualTo(array));
+        Assert.That(swapCount, Is.EqualTo(0));
+    }
+    
+    [Test]
+    public void WhenLeftChildIsLarger_ShouldSwapWithLeft()
+    {
+        int ifCount = 0, swapCount = 0;
+        int[] array = [5, 10, 3];
+
+        var result = HeapSortAlgorithm.Heapify(array, 3, 0, ref ifCount, ref swapCount);
+
+        Assert.That(result[0], Is.EqualTo(10));
+        Assert.That(result[1], Is.EqualTo(5));
+        Assert.That(result[2], Is.EqualTo(3));
+        Assert.That(swapCount, Is.EqualTo(1));
+    }
+
+    [Test]
+    public void WhenRightChildIsLarger_ShouldSwapWithRight()
+    {
+        int ifCount = 0, swapCount = 0;
+        int[] array = [5, 3, 10];
+
+        var result = HeapSortAlgorithm.Heapify(array, 3, 0, ref ifCount, ref swapCount);
+
+        Assert.That(result[0], Is.EqualTo(10));
+        Assert.That(result[2], Is.EqualTo(5));
+        Assert.That(swapCount, Is.EqualTo(1));
+    }
+    
+    [Test]
+    public void WhenCalledOnMiddleElement_ShouldOnlyAffectSubtree()
+    {
+        int ifCount = 0, swapCount = 0;
+        int[] array = [30, 5, 10, 2, 8];
+
+        var result = HeapSortAlgorithm.Heapify(array, 5, 1, ref ifCount, ref swapCount);
+
+        Assert.That(result[1], Is.EqualTo(8));
+        Assert.That(result[4], Is.EqualTo(5));
+        Assert.That(swapCount, Is.EqualTo(1));
+    }
+    
+    [Test]
+    public void HeapifyOnLargeArray_ShouldWorkCorrectly()
+    {
+        int ifCount = 0, swapCount = 0;
+        int[] array = [1, 3, 5, 7, 9, 2, 4, 6, 8, 0];
+
+        var result = HeapSortAlgorithm.Heapify(array, array.Length, 0, ref ifCount, ref swapCount);
+
         Assert.That(result[0], Is.GreaterThanOrEqualTo(result[1]));
         Assert.That(result[0], Is.GreaterThanOrEqualTo(result[2]));
-        Assert.That(ifCount, Is.EqualTo(6));
-        Assert.That(swapCount, Is.EqualTo(1));
+        Assert.That(swapCount, Is.GreaterThan(0));
     }
 }
--- HeapSort.Tests/SwapTests.cs
+++ HeapSort.Tests/SwapTests.cs
@@ -15,5 +15,29 @@ namespace HeapSort.Tests
             Assert.That(result.Item2, Is.EqualTo(1));
             Assert.That(swapCount, Is.EqualTo(1));
         }
+        
+        [Test]
+        public void SwapSameValues_ShouldStillIncrementSwapCount()
+        {
+            var swapCount = 0;
+
+            var result = HeapSortAlgorithm.Swap(5, 5, ref swapCount);
+
+            Assert.That(result.Item1, Is.EqualTo(5));
+            Assert.That(result.Item2, Is.EqualTo(5));
+            Assert.That(swapCount, Is.EqualTo(1));
+        }
+        
+        [Test]
+        public void MultipleSwaps_ShouldAccumulateSwapCount()
+        {
+            var swapCount = 0;
+
+            HeapSortAlgorithm.Swap(1, 2, ref swapCount);
+            HeapSortAlgorithm.Swap(3, 4, ref swapCount);
+            HeapSortAlgorithm.Swap(5, 6, ref swapCount);
+
+            Assert.That(swapCount, Is.EqualTo(3));
+        }
     }
 }
